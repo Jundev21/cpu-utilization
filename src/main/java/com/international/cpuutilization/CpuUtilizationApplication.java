711fe95c2ec7627e2d17f74814792a2f9b9847ec
Junrae Kim <kim34@cougars.csusm.edu>
2024-05-22 01:07:39 +0900

    Initial commit

---
+++ src/main/java/com/international/cpuutilization/CpuUtilizationApplication.java
@@ -0,0 +1,13 @@
+package com.international.cpuutilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuUtilizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuUtilizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/international/cpuutilization/CpuUtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.international.cpuutilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuUtilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
