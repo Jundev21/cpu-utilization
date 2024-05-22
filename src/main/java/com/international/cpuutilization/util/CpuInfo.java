7389a6b6fa6838e58d4b34a2c66de9a12b562b9e
jun <jundev21@gmail.com>
2024-05-22 23:24:33 +0900

    [feat] cpu 클래스 작성

---
+++ src/main/java/com/international/cpuutilization/util/CpuInfo.java
@@ -0,0 +1,28 @@
+package com.international.cpuutilization.util;
+
+import java.lang.management.ManagementFactory;
+
+import org.springframework.stereotype.Component;
+
+import com.sun.management.OperatingSystemMXBean;
+
+@Component
+public class CpuInfo {
+	private final OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+	//	시스템의 CPU 상태 확인
+	// 0 일 경우는 사요량이 0 이고 100 일경우는 1로 나타낸다. 따라서
+	// cpu 사용량은 퍼센테이지로 나타내기 때문에 *100 을 해서 퍼센테이지로 나타낸다
+	public double getCpuUsage() {
+		return Math.floor(this.operatingSystemMXBean.getCpuLoad()*100/100.0);
+	}
+	public double getJvmUsage() {
+		return this.operatingSystemMXBean.getProcessCpuLoad()*100;
+	}
+	public double getFreeMemorySpace() {
+		return (double)this.operatingSystemMXBean.getFreeMemorySize() /1024/1024/1024;
+	}
+	public double getTotalMemorySize() {
+		return (double)this.operatingSystemMXBean.getTotalMemorySize() /1024/1024/1024;
+	}
+}
