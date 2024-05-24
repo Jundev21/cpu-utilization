171b490e8713beaf3b489dadcc721adb361beb05
jun <jundev21@gmail.com>
2024-05-25 04:05:20 +0900

    [update] expression 업데이트

--- src/main/java/com/international/cpuutilization/domain/repository/QueryDslExpressions.java
+++ src/main/java/com/international/cpuutilization/domain/repository/QueryDslExpressions.java
@@ -15,18 +15,11 @@ import lombok.Getter;
 @Getter
 @Component
 public class QueryDslExpressions {
-
 	private final QCpuUtilizationEntity cpuEntity = QCpuUtilizationEntity.cpuUtilizationEntity;
-	private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분");
-	private final DateTimeFormatter dt = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
- 	private final 	NumberTemplate<Integer> minutePath = Expressions.numberTemplate(Integer.class, "extract(minute from {0})", cpuEntity.createdDate);
-	private final NumberTemplate<Integer> hourPath = Expressions.numberTemplate(Integer.class, "extract(hour from {0})", cpuEntity.createdDate);
-	//sql 문
 	private final DateTemplate<LocalDate> date = Expressions.dateTemplate(LocalDate.class, "date_format({0}, {1})", cpuEntity.createdDate,"%Y-%m-%d");
-	//h2
-	private final DateTemplate<LocalDate> datePath = Expressions.dateTemplate(LocalDate.class, "FORMATDATETIME({0}, {1})", cpuEntity.createdDate,"yyyy-MM-dd");
-	private final NumberTemplate<Integer> yearPath = Expressions.numberTemplate(Integer.class, "extract(year from {0})", cpuEntity.createdDate);
-	private final NumberTemplate<Integer> monthPath = Expressions.numberTemplate(Integer.class, "extract(month from {0})", cpuEntity.createdDate);
-	private final NumberTemplate<Integer> dayPath = Expressions.numberTemplate(Integer.class, "extract(day from {0})", cpuEntity.createdDate);
-
+	private final NumberTemplate<Integer> yearPath = Expressions.numberTemplate(Integer.class, "YEAR({0})", cpuEntity.createdDate);
+	private final NumberTemplate<Integer> monthPath = Expressions.numberTemplate(Integer.class, "MONTH({0})", cpuEntity.createdDate);
+	private final NumberTemplate<Integer> dayPath = Expressions.numberTemplate(Integer.class, "DAY({0})", cpuEntity.createdDate);
+	private final NumberTemplate<Integer> hourPath = Expressions.numberTemplate(Integer.class, "HOUR({0})", cpuEntity.createdDate);
+	private final NumberTemplate<Integer> minutePath = Expressions.numberTemplate(Integer.class, "MINUTE({0})", cpuEntity.createdDate);
 }
