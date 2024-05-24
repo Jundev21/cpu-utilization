8bd7a8206848a633009e71b8a48116966d850965
jun <jundev21@gmail.com>
2024-05-24 14:32:58 +0900

    [update] controller 분,시,날 컨트롤러 작성

--- src/main/java/com/international/cpuutilization/domain/controller/CpuUtilController.java
+++ src/main/java/com/international/cpuutilization/domain/controller/CpuUtilController.java
@@ -1,14 +1,19 @@
 package com.international.cpuutilization.domain.controller;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.international.cpuutilization.domain.dto.response.SearchDateResponse;
+import com.international.cpuutilization.domain.dto.response.SearchHourResponse;
+import com.international.cpuutilization.domain.dto.response.SearchMinuteResponse;
 import com.international.cpuutilization.domain.entity.CpuUtilizationEntity;
 import com.international.cpuutilization.domain.service.CpuUtilService;
 
@@ -21,24 +26,29 @@ public class CpuUtilController {
 	private final CpuUtilService cpuUtilService;
 
 	@GetMapping("/min")
-	public ResponseEntity<List<CpuUtilizationEntity>> searchCpuUtilByMin(
-		@RequestParam LocalDateTime startDate,
-		@RequestParam LocalDateTime endDate
+	public ResponseEntity<List<SearchMinuteResponse>> searchCpuUtilByMin(
+		@RequestParam     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+		LocalDateTime startDate,
+		@RequestParam     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+		LocalDateTime endDate
 	) {
 	return ResponseEntity.ok(cpuUtilService.searchCpuUtilByMin(startDate,endDate));
 	}
 
 	@GetMapping("/hour")
-	public ResponseEntity<List<CpuUtilizationEntity>> searchCpuUtilByHour(
-		@RequestParam LocalDateTime pickedDay
+	public ResponseEntity<List<SearchHourResponse>> searchCpuUtilByHour(
+		@RequestParam  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+		LocalDateTime pickedDay
 	) {
 		return ResponseEntity.ok(cpuUtilService.searchCpuUtilByDay(pickedDay));
 	}
 
 	@GetMapping("/day")
-	public ResponseEntity<List<CpuUtilizationEntity>> searchCpuUtilByDays(
-		@RequestParam LocalDateTime startDate,
-		@RequestParam LocalDateTime endDate
+	public ResponseEntity<List<SearchDateResponse>> searchCpuUtilByDays(
+		@RequestParam     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+		LocalDateTime startDate,
+		@RequestParam     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+		LocalDateTime endDate
 	) {
 		return ResponseEntity.ok(cpuUtilService.searchCpuByDays(startDate,endDate));
 	}
